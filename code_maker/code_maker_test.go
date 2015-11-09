cb751b8c2e0b3969600b9a858908873d418e6bd6
Travis Yoder <trayoda@gmail.com>
2015-11-08 20:34:15 -0700

    update code maker test to use the CodeMaker struct

--- code_maker/code_maker_test.go
+++ code_maker/code_maker_test.go
@@ -5,38 +5,48 @@ import (
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
+
 	"github.com/trayo/go_mastermind/code_maker"
 )
 
-var _ = Describe("CodeMaker", func() {
+var _ = Describe("code maker", func() {
+	var (
+		codeMaker code_maker.CodeMaker
+		code      string
+	)
+
+	BeforeEach(func() {
+		codeMaker = *code_maker.NewCodeMaker()
+	})
+
 	It("generates a code", func() {
-		code := code_maker.Generate()
+		code = codeMaker.Generate()
 
 		Expect(regexp.MatchString("^[rgby]{4}$", code)).To(BeTrue())
 	})
 
 	It("generates a code 4 colors long", func() {
-		code := code_maker.Generate()
+		code = codeMaker.Generate()
 
 		Expect(len(code)).To(Equal(4))
 	})
 
 	It("generates a new code every time", func() {
-		code := code_maker.Generate()
-		code = code_maker.Generate()
-		code = code_maker.Generate()
+		code = codeMaker.Generate()
+		code = codeMaker.Generate()
+		code = codeMaker.Generate()
 
 		Expect(len(code)).To(Equal(4))
 	})
 
 	It("accepts a specific code", func() {
-		code := code_maker.Generate("rrrr")
+		code = codeMaker.Generate("rrrr")
 
 		Expect(code).To(Equal("rrrr"))
 	})
 
 	It("ignores other arguments", func() {
-		code := code_maker.Generate("yyyy", "bbbb", "gggg")
+		code = codeMaker.Generate("yyyy", "bbbb", "gggg")
 
 		Expect(code).To(Equal("yyyy"))
 	})
