7630d3ee8e6fa61549d0fa2c58d980b93dda24c5
Travis Yoder <trayoda@gmail.com>
2015-11-26 17:36:38 -0700

    update print messages

--- print/print.go
+++ print/print.go
@@ -11,7 +11,7 @@ func WelcomeMessage() {
 	prompt()
 }
 
-func GoodbyeMessage() {
+func QuitMessage() {
 	for i := 0; i < 30; i++ {
 		fmt.Println("byeeee")
 		time.Sleep(30 * time.Millisecond)
@@ -22,10 +22,9 @@ func Instructions() {
 	fmt.Println("\nThe game will generate a sequence with four elements made up of: ")
 	fmt.Println("(r)ed, (g)reen, (b)lue, and (y)ellow. It will look like 'rybg'.")
 	fmt.Println("Your goal is to guess the code in the fewest guesses possible.")
-	fmt.Println("Guesses must be four characters long and only contain 'r', 'g', 'b' and 'y'.")
+	fmt.Println("\nGuesses must be four characters long and only contain 'r', 'g', 'b' and 'y'.")
 	fmt.Println("It doesn't matter if the guess is upper case or lower case.")
-	fmt.Println("The game will then tell you how many elements of your guess match the code.")
-	fmt.Println("It will tell you correct positions and correct colors.")
+	fmt.Println("The game will then tell you correct positions and correct colors.")
 	fmt.Println("Good luck!\n")
 	WhatsNext()
 	prompt()
