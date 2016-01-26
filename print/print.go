a1b3f0410c2533c066ae7096ffb5ef77d4e5224e
Travis Yoder <trayoda@gmail.com>
2016-01-25 19:39:55 -0700

    Make print global so it can be called in the code instead

--- print/print.go
+++ print/print.go
@@ -18,7 +18,6 @@ func NewPrinter(writer io.Writer) Printer {
 }
 
 func (p Printer) WelcomeMessage() {
-	clearScreen()
 	fmt.Fprintln(p.writer, "Welcome to Mastermind!")
 }
 
@@ -27,7 +26,6 @@ func (p Printer) QuitMessage() {
 }
 
 func (p Printer) Instructions() {
-	clearScreen()
 	fmt.Fprintln(p.writer, "The game will generate a sequence with four elements made up of: ")
 	fmt.Fprintln(p.writer, "(r)ed, (g)reen, (b)lue, and (y)ellow. It will look like 'rybg'.")
 	fmt.Fprintln(p.writer, "Your goal is to guess the code in the fewest guesses possible.")
@@ -43,11 +41,10 @@ func (p Printer) UnknownCommand() {
 
 func (p Printer) WhatsNext() {
 	fmt.Fprintln(p.writer, "Would you like to (p)lay read the (i)nstructions or (q)uit?")
-	fmt.Fprintln(p.writer, "> ")
+	fmt.Fprint(p.writer, "> ")
 }
 
 func (p Printer) GameStart() {
-	clearScreen()
 	fmt.Fprintln(p.writer, "I have generated a random code four characters in length using the letters:")
 	fmt.Fprintln(p.writer, "'r', 'g', 'b' and 'y'")
 	fmt.Fprintln(p.writer, "\nTry and guess the code by providing input like: 'rrrr'")
@@ -55,7 +52,7 @@ func (p Printer) GameStart() {
 
 func (p Printer) EnterAGuess() {
 	fmt.Fprintln(p.writer, "Enter your guess:")
-	fmt.Fprintln(p.writer, "> ")
+	fmt.Fprint(p.writer, "> ")
 }
 
 func (p Printer) ThanksForPlaying() {
@@ -63,7 +60,7 @@ func (p Printer) ThanksForPlaying() {
 	fmt.Fprintln(p.writer, "Remember, winners don't do drugs.\n")
 }
 
-func clearScreen() {
+func (p Printer) ClearScreen() {
 	c := exec.Command("clear")
 	c.Stdout = os.Stdout
 	c.Run()
