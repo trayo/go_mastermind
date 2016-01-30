6531ae072add6fef7f08eb471f24f356336ef3e0
Travis Yoder <trayoda@gmail.com>
2016-01-29 22:01:14 -0700

    Add test for visiting the instructions
    - Also add command sequence functions for readability

--- mastermind_test.go
+++ mastermind_test.go
@@ -3,6 +3,7 @@ package main_test
 import (
 	"bufio"
 	"bytes"
+	"strings"
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
@@ -30,12 +31,39 @@ var _ = Describe("Main CLI interaction", func() {
 		printer = print.NewPrinter(buffer)
 	})
 
-	It("can quit the game", func() {
-		stdin.WriteString("q\n")
+	Context("when providing input", func() {
+		It("can quit the game", func() {
+			commandSequence(stdin, "q")
 
-		main.Run(stdinReader, printer)
+			main.Run(stdinReader, printer)
 
-		Eventually(buffer).Should(gbytes.Say("Welcome"))
-		Eventually(buffer).Should(gbytes.Say("byeee"))
+			Eventually(buffer).Should(gbytes.Say("Welcome"))
+			Eventually(buffer).Should(gbytes.Say("byeee"))
+		})
+
+		It("can visit the instructions page", func() {
+			commandSequence(stdin, "i", "q")
+
+			main.Run(stdinReader, printer)
+
+			Eventually(buffer).Should(gbytes.Say("Welcome"))
+			Eventually(buffer).Should(gbytes.Say("generate a sequence"))
+			Eventually(buffer).Should(gbytes.Say("byeee"))
+		})
+
+		It("can visit the instructions page multiple times", func() {
+			commandSequence(stdin, "i", "i", "q")
+
+			main.Run(stdinReader, printer)
+
+			Eventually(buffer).Should(gbytes.Say("Welcome"))
+			Eventually(buffer).Should(gbytes.Say("generate a sequence"))
+			Eventually(buffer).Should(gbytes.Say("generate a sequence"))
+			Eventually(buffer).Should(gbytes.Say("byeee"))
+		})
 	})
 })
+
+func commandSequence(stdin *bytes.Buffer, args ...string) {
+	stdin.WriteString(strings.Join(args, "\n"))
+}
